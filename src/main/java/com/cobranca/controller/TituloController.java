361eb9bdd0fc944f3ee47458cc09b4938db36b4d
Jaqueline <jaquelinesv90@gmail.com>
2022-03-19 16:46:35 -0300

    envio de dados para o servidor, cadastro de um novo titulo, monstrando a mensagem de sucesso

--- src/main/java/com/cobranca/controller/TituloController.java
+++ src/main/java/com/cobranca/controller/TituloController.java
@@ -1,14 +1,21 @@
 package com.cobranca.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.cobranca.model.Titulo;
+import com.cobranca.repository.Titulos;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+	
+	@Autowired
+	private Titulos titulos;
+	
 	
 	@RequestMapping("/novo")
 	public String novo() {
@@ -16,8 +23,11 @@ public class TituloController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo) {
-		System.out.println("chegou");
-		return "CadastroTitulo";
+	public ModelAndView salvar(Titulo titulo) {
+		
+		titulos.save(titulo);
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem","Título salvo com sucesso!");
+		return mv;
 	}
 }
