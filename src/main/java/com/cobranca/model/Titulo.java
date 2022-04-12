f6a8b21ad2c9fb033a3354bbb805bf92e70ffffa
Jaqueline <jaquelinesv90@gmail.com>
2022-04-12 15:14:26 -0300

    listando os títulos e adicionando estilos aos status

--- src/main/java/com/cobranca/model/Titulo.java
+++ src/main/java/com/cobranca/model/Titulo.java
@@ -75,5 +75,33 @@ public class Titulo {
 		this.status = status;
 	}
 	
+	public boolean isPendente() {
+		return StatusTitulo.PENDENTE.equals(this.status);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Titulo other = (Titulo) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
 
 }
