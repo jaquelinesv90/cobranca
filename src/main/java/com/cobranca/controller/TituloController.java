f2cc1bb52604aaac70dba4e1213b9a2554b7658c
Jaqueline <jaquelinesv90@gmail.com>
2022-04-22 19:31:23 -0300

    mantendo os valores no formulário

--- src/main/java/com/cobranca/controller/TituloController.java
+++ src/main/java/com/cobranca/controller/TituloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cobranca.model.StatusTitulo;
 import com.cobranca.model.Titulo;
@@ -34,16 +35,15 @@ public class TituloController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+	public String salvar(@Validated Titulo titulo, Errors errors,RedirectAttributes attributes) {
 		
 		if(errors.hasErrors()) {
-			return mv;
+			return "CadastroTitulo";
 		}
 		
 		titulos.save(titulo);
-		mv.addObject("mensagem","Título salvo com sucesso!");
-		return mv;
+		attributes.addFlashAttribute("mensagem","Título salvo com sucesso!");
+		return "redirect:/titulos/novo";
 	}
 	
 	@ModelAttribute("todosStatusTitulo")
@@ -60,6 +60,4 @@ public class TituloController {
 		mv.addObject("titulos",todosTitulos);
 		return mv;
 	}
-	
-
 }
